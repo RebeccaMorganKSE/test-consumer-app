47d07dfe5af6f2e330d78bba0b4254b2cf8d7e6e
local_morganr <local_morganr@STM298421.UniNet.unisa.edu.au>
2022-05-31 10:17:33 +0930

    initial commit

---
+++ src/test/java/com/aime/kafkaconsumer256/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aime.kafkaconsumer256;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
